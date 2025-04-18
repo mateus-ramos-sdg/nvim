ec35220712d3d15c03678a86fa14ae21a6f791b9
Mateus Ramos <ramosm@SDGNB1265.sdg.local>
2025-04-18 14:56:26 +0200

    Added plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,18 +3,43 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-    {
-        "MeanderingProgrammer/render-markdown.nvim",
-        dependencies = {
-          "nvim-treesitter/nvim-treesitter",
-          "nvim-tree/nvim-web-devicons", -- or 'echasnovski/mini.icons' if you prefer
-        },
-        config = function()
-          require("render-markdown").setup({
-            -- Optional: specify file types to enable rendering
-            file_types = { "markdown", "quarto" },
-          })
-        end,
-        ft = { "markdown", "quarto" }, -- Lazy-load on these file types
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons', -- or 'echasnovski/mini.icons' if you prefer
+    },
+    config = function()
+      require('render-markdown').setup {
+        -- Optional: specify file types to enable rendering
+        file_types = { 'markdown', 'quarto' },
       }
+    end,
+    ft = { 'markdown', 'quarto' }, -- Lazy-load on these file types
+  },
+  {
+    'brenton-leighton/multiple-cursors.nvim',
+    version = '*', -- Use the latest tagged version
+    opts = {}, -- This causes the plugin setup function to be called
+    keys = {
+      { '<C-j>', '<Cmd>MultipleCursorsAddDown<CR>', mode = { 'n', 'x' }, desc = 'Add cursor and move down' },
+      { '<C-k>', '<Cmd>MultipleCursorsAddUp<CR>', mode = { 'n', 'x' }, desc = 'Add cursor and move up' },
+
+      { '<C-Up>', '<Cmd>MultipleCursorsAddUp<CR>', mode = { 'n', 'i', 'x' }, desc = 'Add cursor and move up' },
+      { '<C-Down>', '<Cmd>MultipleCursorsAddDown<CR>', mode = { 'n', 'i', 'x' }, desc = 'Add cursor and move down' },
+
+      { '<C-LeftMouse>', '<Cmd>MultipleCursorsMouseAddDelete<CR>', mode = { 'n', 'i' }, desc = 'Add or remove cursor' },
+
+      { '<Leader>m', '<Cmd>MultipleCursorsAddVisualArea<CR>', mode = { 'x' }, desc = 'Add cursors to the lines of the visual area' },
+
+      { '<Leader>a', '<Cmd>MultipleCursorsAddMatches<CR>', mode = { 'n', 'x' }, desc = 'Add cursors to cword' },
+      { '<Leader>A', '<Cmd>MultipleCursorsAddMatchesV<CR>', mode = { 'n', 'x' }, desc = 'Add cursors to cword in previous area' },
+
+      { '<Leader>d', '<Cmd>MultipleCursorsAddJumpNextMatch<CR>', mode = { 'n', 'x' }, desc = 'Add cursor and jump to next cword' },
+      { '<Leader>D', '<Cmd>MultipleCursorsJumpNextMatch<CR>', mode = { 'n', 'x' }, desc = 'Jump to next cword' },
+
+      { '<Leader>l', '<Cmd>MultipleCursorsLock<CR>', mode = { 'n', 'x' }, desc = 'Lock virtual cursors' },
+    },
+  },
+  { 'nvim-tree/nvim-web-devicons', opts = {} },
 }
